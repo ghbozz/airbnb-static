1f3470a8395938013cc052c7b2b913c2d8f7ec58
romain sanson <romain.sanson@hey.com>
2022-08-15 17:47:23 +0200

    done

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'flats/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get "/flats", to: "flats#index"
+  get "/flats/:id", to: "flats#show", as: :flat
+end
